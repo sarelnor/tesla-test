eed76b2ce4cb8478767cf29413a9cc8204a0663f
sarnor-iths <sara.nordstrand@iths.se>
2024-04-11 11:22:06 +0200

    created Section.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,54 @@
-import "./App.css";
-import "bootstrap/dist/css/bootstrap-grid.min.css";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Navbar";
-import Navbar from "react-bootstrap/Navbar";
-import { Link, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import { useCarContext } from "./context/CarContext";
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { Link, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import { useCarContext } from './context/CarContext'
+ 
 function App() {
-  const { cars } = useCarContext();
-
-  return (
-    <>
-      <div className="App">
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand>
-              <img
-                className="logo"
-                src="/images/0x0-Tesla_Wordmark_20_Black.png"
-                alt="logo"
-              />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav id="Nav.Link" className="me-auto">
-                {/* Här kommer vi sedan att hämta kontext */}
-                {cars &&
-                  cars.map((car: string, index: number) => (
-                    <li key={index}>
-                      <Link to={"/"}>{car}</Link>
-                    </li>
-                  ))}
-              </Nav>
-              <Nav>
-                <Link id="Nav.Link" to={"/"}>
-                  Home
-                </Link>
-                <Link id="Nav.Link" to={"/about"}>
-                  About
-                </Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-        {/* Lägger in routes från react-router-dom med react-bootstrap */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </>
-  );
+    const { cars } = useCarContext()
+ 
+    return (
+        <div className="App">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand>
+                        <img
+                            className="logo"
+                            src="/images/0x0-Tesla_Wordmark_20_Black.png"
+                            alt="logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav id="Nav.Link" className="me-auto">
+                            {cars &&
+                                cars.map((car: string, index: number) => (
+                                    <li key={index}>
+                                        <Link to={'/'}>{car}</Link>
+                                    </li>
+                                ))}
+                        </Nav>
+                        <Nav>
+                            <Link id="Nav.Link" to={'/'}>
+                                Startsidan
+                            </Link>
+                            <Link id="Nav.Link" to={'/about'}>
+                                About
+                            </Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </div>
+    )
 }
-
-export default App;
+ 
+export default App
---
+++ src/components/Section.tsx
@@ -0,0 +1,101 @@
+import styled from "styled-components";
+
+interface SectionProps {
+  title: string;
+  description: string;
+  bgImg: string;
+  LButton: string;
+  RButton: string;
+}
+
+function Section({
+  title,
+  description,
+  bgImg,
+  LButton,
+  RButton,
+}: SectionProps) {
+  return (
+    <div>
+      <Wrap bgImg={bgImg}>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+        <Buttons>
+          <ButtonGroup>
+            <LeftButton>{LButton}</LeftButton>
+            <RightButton>{RButton}</RightButton>
+          </ButtonGroup>
+        </Buttons>
+        <DownArrow>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="black"
+            focusable="false"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+          >
+            <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+          </svg>
+        </DownArrow>
+      </Wrap>
+    </div>
+  );
+}
+
+export default Section;
+
+//Stilsättningen av containers, och endast bilden behöver typbestämmas igen
+
+const Wrap = styled.div<{ bgImg: string }>`
+  background-image: ${(props) => `url("images/${props.bgImg}")`};
+  height: 120vh;
+  display: flex;
+  background-position: center;
+  background-size: cover;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  align-items: center;
+  color: white;
+  text-align: center;
+  padding: 10px 80px;
+  border-radius: 5px;
+  font-size: 14px;
+  cursor: pointer;
+  display: flex;
+  margin: 8px;
+`;
+
+const RightButton = styled(LeftButton)`
+  background-color: rgba(244, 244, 244, 1);
+  color: black;
+`;
+
+const DownArrow = styled.div`
+  height: 40px;
+  margin-top: 20px;
+  margin-bottom: 2vh;
+  width: 40px;
+  animation: animateDown infinite 1.5s;
+`;
+
+const Buttons = styled.div`
+  margin-top: 60vh;
+`;
