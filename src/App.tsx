3954101c096653b21a897dbc1e3b87d29762ce34
sarnor-iths <sara.nordstrand@iths.se>
2024-04-11 09:41:49 +0200

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import Container from 'react-bootstrap'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-    
+    <div></div>
     </>
   )
 }
